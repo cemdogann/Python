75020dd0b3869e786742bf4c8021c84d889369c5
Cem Doğan <cem.dogan@marun.edu.tr>
2020-05-08 19:54:34 +0300

    string_örnekleri.py
    'strip', 'len', 'lower', 'replace', 'count', 'startswith', 'endswith' komutlarının kullanımına örnek

---
+++ string_örnekleri.py
@@ -0,0 +1,34 @@
+message = ' Hello World '
+website = 'www.cemdogan.com'
+subject = 'Python Çalışma Örnekleri'
+
+# 1- 'message' dizisindeki başındaki ve sonundaki boşlukları sil.
+# result = message.strip()
+# uzunluk = len(message) #-> message dizisinde olan karakter sayısını söylüyor (0'dan başlıyor)
+# print(uzunluk)
+# result1 = message[1:12] #-> Başka bir yoldan yapımı
+# print(result1)
+
+
+# 2- 'website' dizisinde, cemdogan harici yazanları sil.
+# result = len(website) # -> dizideki karakter sayısını öğrenip cemdogan kısmına denk gelen karakterleri yazdırma
+# result = website[4:12]
+
+
+# 3- subject dizisindeki büyük karekterleri küçüğe çevir
+# result = subject.lower()
+# result = subject.replace('P', 'p').replace('Ç', 'ç').replace('Ö', 'ö')
+
+
+# 4- website dizinde kaç m harfi olduğunu bul.
+# result = website.count('m')
+#result = website.count('m', 0, 10) # -> 0 ile 10. index arasındaki m harfi sayısını söyler
+
+
+# 5- 'website' dizisi 'www' ile başlayıp '.com' ile bitiyor mu? # -> (Mail doğrulaması yapılabilir)
+# result = website.startswith('www') 
+# result = endswith('.com')
+# Peki her ikisi doğruysa doğru yazdırma?
+
+6- 
+print(result)
