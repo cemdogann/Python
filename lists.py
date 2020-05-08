c645e268d2f85000e58bc8fe5454fde199e3cac0
Cem Doğan <cem.dogan@marun.edu.tr>
2020-05-08 23:28:21 +0300

    lists.py
    Python'da listeler

---
+++ lists.py
@@ -0,0 +1,40 @@
+# 1- "BMW, Opel, Mercedes, Mazda" elemanlarına sahip bir liste oluştur.
+liste = ['BMW', 'Opel', 'Mercedes', 'Mazda']
+
+# 2- Liste kaç elemanlı?
+# liste1 = len(liste)
+
+# 3- Listenin ilk ve son elemanı nedir?
+# liste1 = liste[0] + ' ' + liste[3]
+
+# 4- Mazda değerini Toyota ile değiştir.
+# liste[3] = 'Toyota'
+# liste1 = liste[3]
+
+# 5- Mercedes listenin bir elemanı mıdır?
+# liste1 = 'Mercedes' in liste
+
+# 6- Listenin -2 indeksindeki değer nedir?
+# liste1 = liste[-2]
+
+# 7- Listenin ilk 3 elemanı?
+# liste1 = liste[0:3]
+
+# 8- Listenin son iki elemanı yerine "Toyota" ve "Renault" değerlerini atayın.
+# liste[-1] = 'Renault'
+# liste[-2] = 'Toyota'
+# liste1 = liste
+
+# 9- Listenin üzerine "Audi" ve "Nissan" değerlerini atayın.
+# liste = liste + ['Audi', 'Nissan']
+# liste1 = liste
+
+# 10- Listenin son elemanını sil.
+# del liste[5]
+# liste1 = liste
+
+# 11- Liste elemanlarını tersten yazdır.
+# liste1 = liste[::-1]
+
+print(liste1)
+print(liste)
